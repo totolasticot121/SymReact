d7c86978d0a02380b25e1016b9bb8eb22a91cbb3
Vince <vincefalcao@gmail.com>
2020-04-20 19:59:45 +0100

    setup app and database

---
+++ src/Migrations/Version20200420180300.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200420180300 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE customer ADD user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE customer ADD CONSTRAINT FK_81398E09A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_81398E09A76ED395 ON customer (user_id)');
+        $this->addSql('ALTER TABLE user ADD first_name VARCHAR(255) NOT NULL, ADD last_name VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE customer DROP FOREIGN KEY FK_81398E09A76ED395');
+        $this->addSql('DROP INDEX IDX_81398E09A76ED395 ON customer');
+        $this->addSql('ALTER TABLE customer DROP user_id');
+        $this->addSql('ALTER TABLE user DROP first_name, DROP last_name');
+    }
+}
